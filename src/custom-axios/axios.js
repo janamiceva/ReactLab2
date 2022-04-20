4205ee734445b5422d6131652964335afb83e5b7
janamiceva <93879063+janamiceva@users.noreply.github.com>
2022-04-20 18:52:25 +0200

    Changing links

--- src/custom-axios/axios.js
+++ src/custom-axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9092/api',
+    baseURL: 'https://springapplab2.herokuapp.com/api',
     headers: {
         'Access-Control-Allow-Origin': '*'
     }
